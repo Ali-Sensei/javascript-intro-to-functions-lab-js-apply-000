5593824ca14df39bd28db2df2b3e78a96aadbb4c
brainy-navigator-3708 <talibaliuk@hotmail.com>
2019-05-03 10:25:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,6 @@ function sayHiToGrandma(string)
     return "YES INDEED!"
   if (string === lowercase)
     return "I can't hear you!"
-
+  if (string === "I love you, Grandma.")
+    return "I love you , too."
 }
